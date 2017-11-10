b2667139dfca2db6b78cfe0f91314e536ea62063
gabrielcostasouza <gabrielcostasouza91@gmail.com>
2017-11-09 21:29:03 -0300

    Create DHT_Cooler.ino

---
+++ DHT_Cooler/DHT_Cooler.ino
@@ -0,0 +1,54 @@
+#include <dht.h> //Inclusão da biblioteca do sensor de temperatura
+#define dhtpin A1 // pino a1 como leitor de temperatura do sensor
+#define vent 9 // pino 9 como clock do pwn do ventilador
+float Temperatura = 0; // variavel que armazena temperatura do sensor 
+float Erro;
+int rotacao = 0; // variavel que armazena a rotacao do motor
+int Sp = 25;
+int Kp = 16;
+dht DHT; //Inicializa o sensor como DHT
+ 
+void setup()
+{
+      Serial.begin(9600); // inicia a entrada serial
+      delay(1000); //Delay para o início da execução do programa
+      
+}
+ 
+void loop()
+{
+      
+      DHT.read11(dhtpin); //le as informações do sensor
+      Temperatura = DHT.temperature; // temperatura do sensor = variavel Temperatura
+      Serial.print("Temperatura "); // print temperatura no serial
+      Serial.print(Temperatura); // print serial variavel temperatura
+      Serial.println(" Celsius "); // print celsius no serial
+
+      if (Temperatura >= Sp) // entra na condicao se a temperatua > Stop Point 
+      {
+        onVentilador(); // inicia a função de ligar o ventilador      
+      }
+      
+      else 
+      {
+      offVentilador(); // inicia a funcao offVentilador
+      }
+      
+      delay(500); //Delay de tres segundos entre as leituras da informação
+
+}
+
+void onVentilador ()
+{
+Erro = SP - Temperatura;
+rotacao = 100 - (Erro*Kp);
+constrain(rotacao, 0, 255);
+analogWrite (vent,rotacao);
+
+}
+
+void offVentilador ()
+{
+  rotacao = 0; // desliga o motor 
+  analogWrite (vent,rotacao); // pwm na porta vent com a frequencia da vaariavel rotacao
+}
