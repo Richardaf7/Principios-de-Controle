8db82dee0a42f32a3350421f984446467b9a8e94
gabrielcostasouza <gabrielcostasouza91@gmail.com>
2017-11-21 21:57:46 -0300

    Update DHT_Cooler.ino

--- DHT_Cooler/DHT_Cooler.ino
+++ DHT_Cooler/DHT_Cooler.ino
@@ -6,14 +6,14 @@ float Erro;
 float Erro_Ant;
 float DErro;
 int rotacao = 0; // variavel que armazena a rotacao do motor
-float SP = 25;
-float Kp = 16;
-float Ki = 5;
+int Sp = 25;
+float Kp = 10;
+float Ki = .0005;
 dht DHT; //Inicializa o sensor como DHT
 float T_atual = 0;
 float T_anterior;
 float dt;
-float P, I;
+float P, I, PiT;
 
 void setup()
 {
@@ -25,7 +25,7 @@ void setup()
 void loop()
 {
       Erro_Ant = Erro;
-      Erro = Sp - Teperatura;
+      Erro = Sp - Temperatura;
       DErro = Erro - Erro_Ant;
       
       T_anterior = T_atual;
@@ -34,12 +34,13 @@ void loop()
       
       DHT.read11(dhtpin); //le as informações do sensor
       Temperatura = DHT.temperature; // temperatura do sensor = variavel Temperatura
-      Serial.print("Temperatura "); // print temperatura no serial
+      Serial.print(" Temperatura "); // print temperatura no serial
       Serial.print(Temperatura); // print serial variavel temperatura
       Serial.println(" Celsius "); // print celsius no serial
 
-      if (Temperatura >= SP) // entra na condicao se a temperatua > Stop Point 
+      if (Temperatura >= Sp) // entra na condicao se a temperatua > Stop Point 
       {
+       
         onVentilador(); // inicia a função de ligar o ventilador      
       }
       
@@ -47,18 +48,29 @@ void loop()
       {
       offVentilador(); // inicia a funcao offVentilador
       }
-      
-      delay(500); //Delay de tres segundos entre as leituras da informação
-      
+      //delay(500); //Delay de tres segundos entre as leituras da informação
+     
+     Serial.print(" Rotacao \n"); // print temperatura no serial
+     Serial.print(rotacao); // print serial variavel temperatura
+     Serial.print("\n dt\n "); // print temperatura no serial
+     Serial.print(dt); // print serial variavel temperatura
+     Serial.print(" \n PiT \n "); // print temperatura no serial
+     Serial.print(PiT); // print serial variavel temperatura
+     Serial.print("\n P \n"); // print temperatura no serial
+     Serial.print(P); // print serial variavel temperatura
+     Serial.print("\n I \n" ); // print temperatura no serial
+     Serial.print(I); // print serial variavel temperatura
+     
+     
 }
 
 void onVentilador ()
 {   
-Erro = SP - Temperatura;
+Erro = Sp - Temperatura;
 P = (Erro*Kp);
-I + = Ki * Erro * dt;
-int PI = P + I;
-rotacao = 100 - (PI);
+I = Ki * Erro * dt;
+PiT = P + I ;
+rotacao = 100 - (PiT);
 constrain(rotacao, 0, 255);
 analogWrite (vent,rotacao);
 
