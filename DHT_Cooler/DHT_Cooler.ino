ec60b1482887f7ac2f1b478a40a4fb1adc9be4de
tfalves1 <33531867+tfalves1@users.noreply.github.com>
2017-11-21 20:10:33 -0400

    Update DHT_Cooler.ino

--- DHT_Cooler/DHT_Cooler.ino
+++ DHT_Cooler/DHT_Cooler.ino
@@ -1,4 +1,4 @@
-#include <dht.h> // Inclusão da biblioteca do sensor de temperatura
+#include <dht.h> //Inclusão da biblioteca do sensor de temperatura
 #define dhtpin A1 // pino a1 como leitor de temperatura do sensor
 #define vent 9 // pino 9 como clock do pwn do ventilador
 float Temperatura = 0; // variavel que armazena temperatura do sensor 
@@ -6,28 +6,35 @@ float Erro;
 float Erro_Ant;
 float DErro;
 int rotacao = 0; // variavel que armazena a rotacao do motor
-int SP = 25;
-int Kp = 16;
-int Ki = 5;
-dht DHT; // Inicializa o sensor como DHT
-int T_atual = 0;
-int T_anterior;
-int dt;
-int P;
+float SP = 25;
+float Kp = 16;
+float Ki = 5;
+dht DHT; //Inicializa o sensor como DHT
+float T_atual = 0;
+float T_anterior;
+float dt;
+float P, I;
 
 void setup()
 {
       Serial.begin(9600); // inicia a entrada serial
-      delay(1000); // Delay para o início da execução do programa
+      delay(1000); //Delay para o início da execução do programa
       
 }
  
 void loop()
 {
+      Erro_Ant = Erro;
+      Erro = Sp - Teperatura;
+      DErro = Erro - Erro_Ant;
       
-      DHT.read11(dhtpin); // le as informações do sensor
+      T_anterior = T_atual;
+      T_atual = millis();
+      dt = T_atual - T_anterior;
+      
+      DHT.read11(dhtpin); //le as informações do sensor
       Temperatura = DHT.temperature; // temperatura do sensor = variavel Temperatura
-      Serial.print(" Temperatura "); // print temperatura no serial
+      Serial.print("Temperatura "); // print temperatura no serial
       Serial.print(Temperatura); // print serial variavel temperatura
       Serial.println(" Celsius "); // print celsius no serial
 
@@ -41,21 +48,15 @@ void loop()
       offVentilador(); // inicia a funcao offVentilador
       }
       
-      delay(500); // Delay de tres segundos entre as leituras da informação
-      T_anterior = T_atual;
-      T_atual = millis();
-      dt = T_atual - T_anterior;
+      delay(500); //Delay de tres segundos entre as leituras da informação
+      
 }
 
 void onVentilador ()
-{
-Erro_Ant = Erro;
-Erro = Sp - Teperatura;
-DErro = Erro - Erro_Ant;
-      
+{   
 Erro = SP - Temperatura;
 P = (Erro*Kp);
-int I + = Ki * Erro * dt;
+I + = Ki * Erro * dt;
 int PI = P + I;
 rotacao = 100 - (PI);
 constrain(rotacao, 0, 255);
